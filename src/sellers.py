371abdc28684269822d742222a700defb947eb13
Paulo Fonseca <paguso@cin.ufpe.br>
2019-09-12 17:09:41 -0300

    ukkonen 1st version broken

--- src/sellers.py
+++ src/sellers.py
@@ -1,4 +1,4 @@
-
+import sys
 
 def phi(a,b):
     return 0 if a==b else 1
@@ -22,13 +22,50 @@ def dist(x,y):
     print(d[prev])
     return d[prev][n]
 
+
+
+def sellers(txt, pat, err):
+    m = len(pat)
+    n = len(txt)
+    occ = []
+    d = [ range(m+1), (m+1)*[0] ]
+    if d[0][m] <= err:
+        occ.append(0)
+    prev = 0
+    cur = 1
+    for j in range(1,n+1):
+        for i in range(1,m+1):
+            d[cur][i] = min( d[prev][i-1] + phi(pat[i-1],txt[j-1]) , d[prev][i] + 1, d[cur][i-1] + 1 )
+        #print(d[prev])
+        if d[cur][m] <= err:
+            occ.append(j-1)
+        cur = (cur+1)%2
+        prev = (prev+1)%2
+    return occ
+
     
 
-def main():
-    x="abracadabra"
-    y="acabradaabra"
-    d = dist(x,y)
-    print("d=",d)
+def amain():
+    txt="abadac"
+    pat="cada"
+    err = 2
+    occ = sellers(txt, pat, 2)
+    print(occ)
+
+def main() :
+    filename = sys.argv[1]
+    pat = sys.argv[2]
+    txtfile  = open(filename, "r")
+    err = int(sys.argv[3])
+    count = 0
+    for txt in txtfile:
+        occ = sellers(txt, pat,err)
+        if occ:
+            print(txt)
+            print(occ)
+        count += 1 if occ else 0 #len(occ)
+    txtfile.close()
+    print("Total line with occurrences:",count)
 
 
 
